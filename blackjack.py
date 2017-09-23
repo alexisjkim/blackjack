fb515b964c2b905c78767a25325cdc1c9ab16e60
Alexis Kim <ajkim0630@gmail.com>
2017-09-22 21:06:58 -0700

    added autoplay

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,4 @@
+
 import random
 decks = eval(input("How many decks would you like?"))
 
@@ -20,7 +21,85 @@ win = 0
 loss = 0
 coins = 5000
 
-def blackjack(yourcoins,wins,losses,list):
+def blackjack(yourcoins,wins,losses,list,debug):
+    if debug == True:
+        f = open('log.txt','w')
+        for i in range(10):
+            f.write('\n\n')
+            f.write(str(i))
+            f.write('\n\n')
+            a = list.pop()
+            b = list.pop()
+            dealer1 = list.pop()
+            dealer2 = list.pop()
+            if a == 'A':
+                a = 11
+            if b == 'A':
+                b = 11
+            if dealer1 == 'A':
+                dealer1 = 11
+            if dealer2 == 'A':
+                dealer2 = 11
+            if a == 'J' or a == 'Q' or a == 'K':
+                a = 10
+            if b == 'J' or b == 'Q' or b == 'K':
+                b = 10
+            if dealer1 == 'J' or dealer1 == 'Q' or dealer1 == 'K':
+                dealer1 = 10
+            if dealer2 == 'J' or dealer2 == 'Q' or dealer2 == 'K':
+                dealer2 = 10
+            playersum = a + b
+            dealersum = dealer1 + dealer2
+            f.write('player card:')
+            f.write(str(a))
+            f.write('\nplayer card: ')
+            f.write(str(b))
+            f.write('\ndealer card: ')
+            f.write(str(dealer1))
+            f.write('\ndealer card: ')
+            f.write(str(dealer2))
+            f.write('\n')
+            f.write('playersum:')
+            f.write(str(playersum))
+            f.write('dealersum:')
+            f.write(str(dealersum))
+            f.write('\n')
+            while playersum < 17:
+                hit = list.pop()
+                if hit == 'J' or hit == 'Q' or hit == 'K':
+                    hit = 10
+                if hit == 'A':
+                    hit = 11
+                f.write('player hit and got:')
+                f.write (str(hit))
+                f.write ('\n')
+                playersum += hit
+            while dealersum < 17:
+                hit = list.pop()
+                if hit == 'J' or hit == 'Q' or hit == 'K':
+                    hit = 10
+                if hit == 'A':
+                    hit = 11
+                f.write('\ndealer hit and got:')
+                f.write (str(hit))
+                f.write('\n')
+                dealersum += hit
+            if playersum > 21:
+                f.write ("player busted\n")
+                next
+            elif dealersum > 21:
+                f.write ("dealer busted\n")
+                next
+            elif playersum > dealersum:
+                f.write ("player won\n")
+                next
+            elif playersum < dealersum:
+                f.write ('dealer won\n')
+                next
+            elif playersum == dealersum:
+                f.write('push\n')
+        done = "done"
+        return done
     while True:
         if yourcoins == 0:
             print ("Haha you're out of money lol")
@@ -190,7 +269,7 @@ def blackjack(yourcoins,wins,losses,list):
                 yourcoins += bet
 
 
-a = blackjack(coins,win,loss,luna)
+a = blackjack(coins,win,loss,luna,True)
 print (a)
 '''
 to do list
