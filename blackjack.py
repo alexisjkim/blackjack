90c9a54e6b9477e7a8858e839a93567feb880886
Alexis Kim <ajkim0630@gmail.com>
2017-09-30 20:50:27 -0700

    fixed suit info

--- blackjack.py
+++ blackjack.py
@@ -1,16 +1,15 @@
 def deck(n):
-
-
-    onedeck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A',
-               2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A',
-               2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A',
-               2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A']
+    onedeck = []
+    for num in [2, 3, 4]:
+        for suit in ['heart', 'spade','club','diamond']:
+            onedeck.append( (num, suit)  )
     alldecks = []
     for i in range(n):
         alldecks.extend(onedeck)
     return alldecks
 
 def blackjack(debug=False, file=""):
+    round = 1
     losses = 0
     coins = 5000
     wins = 0
@@ -24,65 +23,62 @@ def blackjack(debug=False, file=""):
     random.shuffle(list)
     f = open(file, 'w')
     if debug == True:
+        event = 1
         for i in range(10):
             f.write('\n\n' + str(i)+'\n\n')
             a = list.pop()
             b = list.pop()
             dealer1 = list.pop()
             dealer2 = list.pop()
-            if a == 'A':
+            if a[0] == 'A':
                 f.write("player card"+a+'\n')
-                a = 11
-            if b == 'A':
+                a[0] = 11
+            if b[0] == 'A':
                 f.write("player card"+b+'\n')
-                b = 11
-            if dealer1 == 'A':
+                b[0] = 11
+            if dealer1[0] == 'A':
                 f.write("dealer card:" + dealer1+'\n')
-                dealer1 = 11
-            if dealer2 == 'A':
+                dealer1[0] = 11
+            if dealer2[0] == 'A':
                 f.write("dealer card:"+ dealer2)
-                dealer2 = 11
-            if a == 'J' or a == 'Q' or a == 'K':
+                dealer2[0] = 11
+            if a[0] == 'J' or a[0] == 'Q' or a[0] == 'K':
                 f.write('player card:'+ a+'\n')
-                a = 10
-            if b == 'J' or b == 'Q' or b == 'K':
+                a[0] = 10
+            if b[0] == 'J' or b[0] == 'Q' or b[0] == 'K':
                 f.write('player card:' + b + '\n')
-                b = 10
-            if dealer1 == 'J' or dealer1 == 'Q' or dealer1 == 'K':
+                b[0] = 10
+            if dealer1[0] == 'J' or dealer1[0] == 'Q' or dealer1[0] == 'K':
                 f.write('dealer card:' + dealer1 + '\n')
-                dealer1 = 10
-            if dealer2 == 'J' or dealer2 == 'Q' or dealer2 == 'K':
+                dealer1[0] = 10
+            if dealer2[0] == 'J' or dealer2[0] == 'Q' or dealer2[0] == 'K':
                 f.write('dealer card:' + dealer2 + '\n')
-                dealer2 = 10
-
-            f.write('player card:'+ str(a)+ '\nplayer card: ' + str(b)+'\ndealer card: ')
-            f.write(str(dealer1)+'\ndealer card: '+str(dealer2)+'\n\n')
-            playersum = a + b
-            dealersum = dealer1 + dealer2
-            f.write('playersum:')
-            f.write(str(playersum))
-            f.write('dealersum:')
-            f.write(str(dealersum))
+                dealer2[0] = 10
+            f.write('Round '+str(round)+', event '+str(event)+': '+'      player cards are: '+str(a)+', '+str(b)+'   dealer cards are '+str(dealer1)+' and ?')
+            playersum = a[0] + b[0]
+            dealersum = dealer1[0] + dealer2[0]
+            f.write('\nplayersum:'+str(playersum))
+            f.write('   dealersum: '+str(dealersum)+'\n')
             while playersum < 17:
                 hit = list.pop()
-                if hit == 'J' or hit == 'Q' or hit == 'K':
-                    hit = 10
-                if hit == 'A':
-                    hit = 11
+                if hit[0] == 'J' or hit[0] == 'Q' or hit[0] == 'K':
+                    hit[0] = 10
+                if hit[0] == 'A':
+                    hit[0] = 11
                 f.write('player hit and got:')
                 f.write (str(hit))
                 f.write ('\n')
-                playersum += hit
+                playersum += hit[0]
             while dealersum < 17:
                 hit = list.pop()
-                if hit == 'J' or hit == 'Q' or hit == 'K':
-                    hit = 10
-                if hit == 'A':
-                    hit = 11
+                if hit[0] == 'J' or hit[0] == 'Q' or hit[0] == 'K':
+                    hit[0] = 10
+                if hit[0] == 'A':
+                    hit[0] = 11
                 f.write('\ndealer hit and got:')
                 f.write (str(hit))
                 f.write('\n')
-                dealersum += hit
+                dealersum += hit[0]
             if playersum > 21:
                 f.write ("player busted\n")
                 next
@@ -107,7 +103,7 @@ def blackjack(debug=False, file=""):
         print ("wins, losses:", wins, losses)
         print ('\nNew Game')
         print ("You have",coins,"coins.\n")
-        f.write("wins, losses:"+ wins+ losses)
+        f.write("wins, losses:"+ str(wins)+ str(losses))
         print('\nNew Game')
         print("You have", coins, "coins.\n")
         while True:
@@ -178,14 +174,14 @@ def blackjack(debug=False, file=""):
                 break
             if x == 'double down' or x == 'dd' or x == 'd':
                 bet = bet * 2
-                hit = list.pop()
-                if hit == 'J' or hit == 'Q' or hit == 'K':
-                    hit = 10
-                if hit == 'A':
-                    hit = 11
-                print ('Your new card:', hit)
-                print('Your new card:' + hit)
-                playersum += hit
+                hit[0] = list.pop()
+                if hit[0] == 'J' or hit[0] == 'Q' or hit[0] == 'K':
+                    hit[0] = 10
+                if hit[0] == 'A':
+                    hit[0] = 11
+                print ('Your new card:', hit[0])
+                print('Your new card:' + hit[0])
+                playersum += hit[0]
                 print ("Your new total:", playersum)
                 break
             if x == 'cheat' or x == 'c':
@@ -193,11 +189,11 @@ def blackjack(debug=False, file=""):
             if x == 'h' or x == 'hit':
                 hit = list.pop()
                 print ("your new card:",hit)
-                if hit == 'J' or hit == 'Q' or hit == 'K':
-                    hit = 10
-                if hit == 'A':
-                    hit = 11
-                playersum += hit
+                if hit[0] == 'J' or hit[0] == 'Q' or hit[0] == 'K':
+                    hit[0] = 10
+                if hit[0] == 'A':
+                    hit[0] = 11
+                playersum += hit[0]
                 print ('your new total:', playersum)
             if x == 's' or x == 'stand':
                 break
@@ -209,31 +205,31 @@ def blackjack(debug=False, file=""):
             hit = list.pop()
             hit2 = list.pop()
             print (a,hit, '\n',b,hit2)
-            if hit == 'J' or hit == 'K' or hit == 'Q':
-                hit = 10
-            if hit2 == 'J' or hit2 == 'Q' or hit2 == 'K':
-                hit = 10
-            if hit == 'A':
-                hit = 11
-            if hit2 == 'A':
-                hit2 = 11
-            split1 = a + hit
-            split2 = b + hit2
+            if hit[0] == 'J' or hit[0] == 'K' or hit[0] == 'Q':
+                hit[0] = 10
+            if hit[0] == 'J' or hit[0] == 'Q' or hit[0] == 'K':
+                hit[0] = 10
+            if hit[0] == 'A':
+                hit[0] = 11
+            if hit2[0] == 'A':
+                hit2[0] = 11
+            split1 = a + hit[0]
+            split2 = b + hit2[0]
             bet = bet * 2
             while True:
                 x = input("hit or stand?")
                 if x == 'h' or x == 'hit':
                     hit = list.pop()
                     print ("your new card is", hit)
-                    if hit == 'Q' or hit == 'K' or hit == 'J':
-                        hit = 10
-                    if hit == 'A':
-                        hit = 11
+                    if hit[0] == 'Q' or hit[0] == 'K' or hit[0] == 'J':
+                        hit[0] = 10
+                    if hit[0] == 'A':
+                        hit[0] = 11
                     whichone = input ('do you want it on the first or second one')
                     if whichone == '1':
-                        split1 += hit
+                        split1 += hit[0]
                     elif whichone == '2':
-                        split2 += hit
+                        split2 += hit[0]
                 if x == 's' or x == 'stand':
                     break
                 if split1 > 21:
@@ -262,11 +258,11 @@ def blackjack(debug=False, file=""):
             list.pop()
             print ("the dealer hit and got:",dealerhit)
             f.write("the dealer hit and got:" + dealerhit)
-            if dealerhit == 'J' or dealerhit == 'Q' or dealerhit == 'K':
-                dealerhit = 10
-            if dealerhit == 'A':
-                dealerhit = 11
-            dealersum += dealerhit
+            if dealerhit[0] == 'J' or dealerhit[0] == 'Q' or dealerhit[0] == 'K':
+                dealerhit[0] = 10
+            if dealerhit[0] == 'A':
+                dealerhit[0] = 11
+            dealersum += dealerhit[0]
         print ('dealer total:', dealersum)
         if dealersum > 21:
             print ("dealer busted")
